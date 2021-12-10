a60452b1eaa8b4ed0cb84ac3e2b12a626e28f40e
John Brewer <jbrewer@jera.com>
2021-12-09 20:53:25 -0800

    Initial checkin of Unreal third person template

---
+++ Source/AvatarChat/AvatarChatGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AvatarChatGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AAvatarChatGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AAvatarChatGameMode();
+};
+
+
+
